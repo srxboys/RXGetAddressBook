028d30abe4004833367c16c7bccaef38ab4dd67a
sunmengxin <sunmengxin@readtv.cn>
2016-04-21 16:38:05 +0800

    Initial Commit

---
+++ RXGetAddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RXGetAddressBook
+//
+//  Created by srx on 16/4/21.
+//  Copyright © 2016年 srxboys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
